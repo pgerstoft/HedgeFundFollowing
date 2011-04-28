ae7565ca0965b9208cfccf98ef9c2de44ab5c6c5
Philip Gerstoft <philipgerstoft@gmail.com>
2011-04-28 11:11:49 -0700

    Added filter by Fund Return capability

--- src/DataAnalysis.java
+++ src/DataAnalysis.java
@@ -10,12 +10,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.TreeSet;
 
+import weka.attributeSelection.ASSearch;
+import weka.attributeSelection.BestFirst;
+import weka.attributeSelection.CfsSubsetEval;
+import weka.attributeSelection.GainRatioAttributeEval;
 import weka.classifiers.*;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.bayes.NaiveBayesUpdateable;
 import weka.classifiers.functions.Logistic;
 import weka.classifiers.functions.SimpleLogistic;
+import weka.classifiers.meta.AttributeSelectedClassifier;
 import weka.classifiers.trees.*;
 import weka.core.Instance;
 import weka.core.Instances;
@@ -24,6 +31,7 @@ import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 
 import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
 import weka.filters.unsupervised.instance.NonSparseToSparse;
  
 
@@ -37,29 +45,40 @@ public class DataAnalysis {
 		Instances train  = new Instances(new BufferedReader(new FileReader("designMatrixTrain.arff"))); 
 		train.setClassIndex(train.numAttributes() - 1);
 	    
-	    ArffLoader loaderTest = new ArffLoader();
-	    Instances test = new Instances(new BufferedReader(new FileReader("designMatrixTest.arff")));//loaderTest.getStructure();
-			test.setClassIndex(test.numAttributes() - 1);
+	    Instances test = new Instances(new BufferedReader(new FileReader("designMatrixTest.arff")));
+		test.setClassIndex(test.numAttributes() - 1);
+
+//		AttributeSelectedClassifier classifier = new AttributeSelectedClassifier();
+//		CfsSubsetEval eval = new CfsSubsetEval();		
+//		BestFirst search =  new BestFirst();
+//		
+//		SimpleLogistic base = new SimpleLogistic();
+//		
+//		classifier.setClassifier(base);
+//		classifier.setEvaluator(eval);
+//		classifier.setSearch(search);
 		
 	    
+	    
+//		SimpleLogistic classifier = new SimpleLogistic();
+//		Classifier classifier = new RandomForest();
+		Classifier classifier = new NaiveBayes();
 		
-		SimpleLogistic classify = new SimpleLogistic();
-//		Classifier classify = new RandomForest();
+		System.out.println("Build Train");
 
-		
-			classify.buildClassifier(train);
+		classifier.buildClassifier(train);
 			 // evaluate classifier and print some statistics
 			System.out.println("Evaluating");
-			Evaluation eval = new Evaluation(train);
-			 eval.evaluateModel(classify, test);
-			 System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+			Evaluation evaluation = new Evaluation(test);
+			evaluation.evaluateModel(classifier, test);
+//			 eval.evaluateModel(classify, test);
+			 System.out.println(evaluation.toSummaryString("\nResults\n======\n", false));
 		
 		int correct = 0;
 		int numInstances = 0;
 		try {
 		
 		Instance currentTest  = null;
-		System.out.println(loaderTest == null);
 		for(Enumeration e = test.enumerateInstances(); e.hasMoreElements(); ){
 			currentTest = (Instance) e.nextElement();
 				numInstances ++;
@@ -84,8 +103,8 @@ public class DataAnalysis {
 		//TODO move file before you write, input file to makeDesigntemp
 		String tempTrainFile = "temp/tempTrainDesignMatrix.csv";
 		String tempTestFile  = "temp/tempTestDesignMatrix.csv";
-		String designMatrixTrain = "designMatrixTrain.arff";
-		String designMatrixTest = "designMatrixTest.arff";
+		String designMatrixTrain = "designMatrixTrain"+ predictionQuarter.toString().replace('/', '_') + "_" + numTrainingQuarters + ".arff";
+		String designMatrixTest = "designMatrixTest"+ predictionQuarter.toString().replace('/', '_') + "_" + numTrainingQuarters + ".arff";
 		//set allParts of writeable to true
 		new File(System.getProperty("user.dir")+"/temp/").setWritable(true);
 		
@@ -95,22 +114,23 @@ public class DataAnalysis {
 		
 		System.out.println("Starting Quarter " +  startQuarter);
 		
-//		System.out.println("Storing Database values into csv file...");
-//		new File(tempTrainFile).delete();
-//		DB.makeDesignTemp(System.getProperty("user.dir")+"/" + tempTrainFile, startQuarter.getPreviousQuarter(), predictionQuarter);
-//		new File(tempTestFile).delete();
-//		DB.makeDesignTemp(System.getProperty("user.dir")+"/" + tempTestFile, predictionQuarter.getPreviousQuarter(), predictionQuarter.getNextQuarter());		
-//		System.out.println("Pivoting table...");
-//		
 		boolean isTrainingDesignMatrix = true;
-		System.out.println("Training Design Matrix...");
-		writeDesignMatrixARFF(tempTrainFile, tempTestFile, designMatrixTrain, isTrainingDesignMatrix);
-		System.out.println("Test Design Matrix...");
-		writeDesignMatrixARFF(tempTrainFile, tempTestFile, designMatrixTest, !isTrainingDesignMatrix);
 		
+		System.out.println("Storing Database values into csv file...");
+		if(! new File(designMatrixTrain).exists()){
+			new File(tempTrainFile).delete();
+			DB.makeDesignTemp(System.getProperty("user.dir")+"/" + tempTrainFile, startQuarter.getPreviousQuarter(), predictionQuarter);
+			System.out.println("Training Design Matrix...");
+			writeDesignMatrixARFF(tempTrainFile, tempTestFile, designMatrixTrain, isTrainingDesignMatrix, startQuarter, numTrainingQuarters );
+		}
+		if(! new File(designMatrixTest).exists()){
+			new File(tempTestFile).delete();
+			DB.makeDesignTemp(System.getProperty("user.dir")+"/" + tempTestFile, predictionQuarter.getPreviousQuarter(), predictionQuarter.getNextQuarter());	
+
+			System.out.println("Test Design Matrix...");
+			writeDesignMatrixARFF(tempTrainFile, tempTestFile, designMatrixTest, !isTrainingDesignMatrix, startQuarter, numTrainingQuarters);
+		}
 		
-//		new File(tempTrainFile).delete();
-//		new File(tempTestFile).delete();
 	}
 	
 	private ArrayList<Double> zeroedOutArrayList(int size) {
@@ -122,123 +142,10 @@ public class DataAnalysis {
 	}
 
 	
-	//write desgin Matrix in sparse arff notation
-	
-	private void writeDesignMatrix(String tempTrainFile, String tempTestFile, String designMatrixFile, boolean isTrainingDesignMatrix){
-//		String designMatrixFileArff1 = designMatrixFile.replace(".csv", ".arff");
-//		csvToArff(designMatrixFile, designMatrixFileArff1);
-//		arffToSparse(designMatrixFileArff1);
-//		if(true)
-//		return;
-		
-		int colNumTicker = 0; //ReadCSVFile.getColNum(line, "TICKER");
-		int colNumQuarter = 1;
-		int colNumCIK = 2; //ReadCSVFile.getColNum(line, "CIK");
-		int colNumPOF = 3; //ReadCSVFile.getColNum(line, "PortionOfFund");
-		int colNumReturn = 4; //ReadCSVFile.getColNum(line,"RET"); //TODO
-		
-		System.out.println("getting CIKS FROM FILE...");
-		
-		//we only want ciks that can be used in the final model
-		TreeSet<CIK> ciks = getCIKSFromFile(tempTestFile, colNumCIK);
-		
-		System.out.println(ciks.size());
-		
-		Hashtable<Quarter, Double> spyReturns = new Hashtable<Quarter, Double>();
-		
-		Hashtable<CIK, Integer> cikToIndex = new Hashtable<CIK, Integer>();
-		int index = 0;
-		for(Object c: ciks.toArray())
-			cikToIndex.put((CIK) c, index++);
-		
-		ArrayList<Double> portionOfFunds = zeroedOutArrayList(cikToIndex.size());
+	//write design Matrix in sparse arff notation
+	private void writeDesignMatrixARFF(String tempTrainFile, String tempTestFile, String designMatrixFile, boolean isTrainingDesignMatrix, Quarter startQuarter, int numQuarters){
 
-		String ticker = null, newTicker = null;
-		Quarter quarter = null, newQuarter = null;
-		double portionOfFund;
-		CIK cik;
-		boolean ret;
-		String line;
-		
-		BufferedReader in = null;
-		BufferedWriter out = null;
-		
-		try{
-			out = new BufferedWriter(new FileWriter(designMatrixFile));
-			
-			if(isTrainingDesignMatrix)
-				in = new BufferedReader(new FileReader(tempTrainFile));
-			else
-				in = new BufferedReader(new FileReader(tempTestFile));
-						
-
-			//header
-			out.write("Ticker, ");
-			out.write("Quarter, ");
-			CIK tempCIK = null;
-			
-			for(Object c : ciks.toArray()){
-				tempCIK = (CIK) c;
-				out.write("POF"+tempCIK.toString()+", ");
-			}
-			
-			out.write("Return \n");
-			int count = 0;
-			while((line = in.readLine()) != null){
-				count++;
-				if(count >  200)
-					break;
-				newTicker = ReadCSVFile.getValue(line, colNumTicker);
-				
-				newQuarter = new Quarter(ReadCSVFile.getValue(line, colNumQuarter));
-				ticker = ticker == null ? newTicker : ticker;
-				quarter = quarter == null ? newQuarter : quarter;
-
-				
-				if(!ticker.equals(newTicker) || !quarter.equals(newQuarter)){ //is new Quarter || is new Ticker
-					if(!spyReturns.containsKey(quarter))
-						spyReturns.put(quarter, DB.getThreeMonthStockReturn(DB.getCusipFromTicker("SPY", quarter), quarter));
-					System.out.println(spyReturns.get(quarter));
-					ret = new Double(ReadCSVFile.getValue(line, colNumReturn)) - spyReturns.get(quarter) > 0 ? true : false;
-										
-					out.write(ticker + ","  + quarter + "," +  portionOfFunds.toString().replaceAll("[\\[\\]]", "") + "," + ret+"\n");
-					
-					portionOfFunds = zeroedOutArrayList(cikToIndex.size());
-					ticker = newTicker;
-					quarter = newQuarter;
-					cik = new CIK(ReadCSVFile.getValue(line, colNumCIK));
-					portionOfFund = new Double(ReadCSVFile.getValue(line, colNumPOF));
-					if(cikToIndex.get(cik) != null)
-						portionOfFunds.set(cikToIndex.get(cik), portionOfFund);
-				}else{
-					cik = new CIK(ReadCSVFile.getValue(line, colNumCIK));
-					portionOfFund = new Double(ReadCSVFile.getValue(line, colNumPOF));
-					if(cikToIndex.get(cik) != null)
-						portionOfFunds.set(cikToIndex.get(cik), portionOfFund);
-				}
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}finally{
-			if(in != null){
-				try {
-					in.close();
-				} catch (IOException e) {
-				}
-			}
-			if(out != null){
-				try {
-					out.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-		String designMatrixFileArff = designMatrixFile.replace(".arff", "2.arff");
-		csvToArff(designMatrixFile, designMatrixFileArff);
-		arffToSparse(designMatrixFileArff);
-	}
-	
-	private void writeDesignMatrixARFF(String tempTrainFile, String tempTestFile, String designMatrixFile, boolean isTrainingDesignMatrix){
+		DB.turnMultipleQueriesOn();
 
 		
 		int colNumTicker = 0; 
@@ -250,19 +157,42 @@ public class DataAnalysis {
 		System.out.println("getting CIKS FROM FILE...");
 		
 		//we only want ciks that can be used in the final model
-//		TreeSet<CIK> ciks = getCIKSFromFile(tempTestFile, colNumCIK);
-		
-		TreeSet<CIK> ciks = getCIKSFromFile(tempTrainFile, colNumCIK);	
+		TreeSet<CIK> ciks = getCIKSFromFile(tempTestFile, colNumCIK);
+//		TreeSet<CIK> ciks = getCIKSFromFile(tempTrainFile, colNumCIK);	
+
+		//Get returns
+		Hashtable<CIK, Double> cikToReturns = computeReturns(ciks, startQuarter, numQuarters);
+
+		//Get spy return for each quarter and over the whole period
+		Hashtable<Quarter, Double> spyReturns = new Hashtable<Quarter, Double>();				
+		Cusip spyCusip = DB.getCusipFromTicker("SPY", startQuarter);
+		Quarter q = startQuarter;
+		double totalSpyReturn = 1;
+		for(int i = 0; i < numQuarters; i++){
+			spyReturns.put(q, DB.getThreeMonthStockReturn(spyCusip, q));
+			totalSpyReturn *= (1 + spyReturns.get(q));
+			q = q.getNextQuarter();
+		}
+		spyReturns.put(q, DB.getThreeMonthStockReturn(spyCusip, q));
+		totalSpyReturn--;
 		
 		System.out.println(ciks.size());
+		System.out.println(totalSpyReturn);
+		System.out.println(cikToReturns);
+		Iterator<CIK> iter = ciks.iterator();
+		while(iter.hasNext()){
+			if(cikToReturns.get(iter.next()) <= totalSpyReturn)
+				iter.remove();
+		}
 		
+		System.out.println(ciks.size());
+
 		TreeSet<String> tickers;
 		if(isTrainingDesignMatrix)
 			tickers = getTickersFromFile(tempTrainFile, colNumTicker);
 		else
 			tickers = getTickersFromFile(tempTestFile, colNumTicker);
 				
-		Hashtable<Quarter, Double> spyReturns = new Hashtable<Quarter, Double>();				
 		
 		
 		Hashtable<Integer, String> portionOfFunds = new Hashtable<Integer, String>();
@@ -286,34 +216,30 @@ public class DataAnalysis {
 				in = new BufferedReader(new FileReader(tempTestFile));
 						
 
-//			if(isTrainingDesignMatrix)
-//				out.write("@relation designMatrixTrain\n");
-//			else
-//				out.write("@relation designMatrixTest\n");
-//			
-//			out.write("@attribute Ticker ");
-//			out.write("{"+ tickers.toString().replaceAll("[\\[\\]]", "")+ "}");
-//			out.write("\n");
-
-			CIK tempCIK = null;
+			if(isTrainingDesignMatrix)
+				out.write("@relation designMatrixTrain\n");
+			else
+				out.write("@relation designMatrixTest\n");
+			
+			out.write("@attribute Ticker ");
+			out.write("{"+ tickers.toString().replaceAll("[\\[\\]]", "")+ "}");
+			out.write("\n");
 			
 			Hashtable<CIK, Integer> cikToIndex = new Hashtable<CIK, Integer>();
 			int index = 0;
 
-			for(Object c : ciks.toArray()){
-				tempCIK = (CIK) c;
-				if(index == 0)
-					System.out.println(tempCIK);
-				cikToIndex.put(tempCIK, index);
+			for(CIK c : ciks){
+
+				cikToIndex.put(c, index);
 				
-//				out.write("@attribute \'POF"+tempCIK.toString()+"\'  numeric\n");
+				out.write("@attribute \'POF"+c.toString()+"\'  numeric\n");
 				index++;
 			}
 			
 			//The predicted variable
-//			out.write("@attribute Class  {true,false}\n");
+			out.write("@attribute Class  {true,false}\n");
 //			
-//			out.write("@data\n");
+			out.write("@data\n");
 			while((line = in.readLine()) != null){
 
 				newTicker = ReadCSVFile.getValue(line, colNumTicker);
@@ -324,13 +250,12 @@ public class DataAnalysis {
 
 				
 				if(!ticker.equals(newTicker) || !quarter.equals(newQuarter)){ //is new Quarter || is new Ticker
-					if(!spyReturns.containsKey(quarter))
-						spyReturns.put(quarter, DB.getThreeMonthStockReturn(DB.getCusipFromTicker("SPY", quarter), quarter));
 					System.out.println(spyReturns.get(quarter));
 					ret = new Double(ReadCSVFile.getValue(line, colNumReturn)) - spyReturns.get(quarter) > 0 ? true : false;
-										
-					out.write("");
-//					out.write("0 " + ticker + ",");
+					
+					if(portionOfFunds.size()>0){					
+					out.write("{");
+					out.write("0 " + ticker + ",");
 
 					if(portionOfFunds.size()>0){
 						ArrayList<Integer> keys = new ArrayList<Integer>();
@@ -339,16 +264,17 @@ public class DataAnalysis {
 						}
 						Collections.sort(keys);
 						for(int key: keys){
-//							out.write((key+1) + " " + portionOfFunds.get(key)  +",");
+							out.write((key+1) + " " + portionOfFunds.get(key)  +",");
 						}
 					}
 					
 					
 					
-					out.write(" " + portionOfFunds.size()+ ",");
-					out.write(""+(new Double(ReadCSVFile.getValue(line, colNumReturn)) - spyReturns.get(quarter)));
-//					out.write((cikToIndex.size()+1)+ " " + ret);
+//					out.write(" " + portionOfFunds.size()+ ",");
+//					out.write(""+(new Double(ReadCSVFile.getValue(line, colNumReturn)) - spyReturns.get(quarter)));
+					out.write((cikToIndex.size()+1)+ " " + ret + "}");
 					out.write(" \n");
+					}
 					portionOfFunds = new Hashtable<Integer, String>();
 					ticker = newTicker;
 					quarter = newQuarter;
@@ -356,14 +282,18 @@ public class DataAnalysis {
 					portionOfFund = new Double(ReadCSVFile.getValue(line, colNumPOF));
 					if(cikToIndex.get(cik) != null){
 						if(portionOfFund > .05)
-							portionOfFunds.put((cikToIndex.get(cik)), ""+ portionOfFund);
+							portionOfFunds.put((cikToIndex.get(cik)), ""+ 1);
+
+//							portionOfFunds.put((cikToIndex.get(cik)), ""+ portionOfFund);
 					}
 				}else{
 					cik = new CIK(ReadCSVFile.getValue(line, colNumCIK));
 					portionOfFund = new Double(ReadCSVFile.getValue(line, colNumPOF));
 					if(cikToIndex.get(cik) != null){
 						if(portionOfFund > .05) 
-							portionOfFunds.put((cikToIndex.get(cik)), ""+ portionOfFund);
+							portionOfFunds.put((cikToIndex.get(cik)), ""+ 1);
+
+//							portionOfFunds.put((cikToIndex.get(cik)), ""+ portionOfFund);
 					}
 				}
 			}
@@ -384,7 +314,7 @@ public class DataAnalysis {
 			}
 		}
 		
-		
+		DB.turnMultipleQueriesOff();
 	}
 	
 //TODO:	public void pivotTable(String file, int top, int[] sides, int y)
@@ -424,7 +354,7 @@ public class DataAnalysis {
 	}
 	
 	
-	//why TreeSet? fast lookup
+	//why TreeSet? no duplicates
 	private static TreeSet<CIK> getCIKSFromFile(String fileName, int colNum){
 		TreeSet<CIK> ciks = new TreeSet<CIK>();
 		BufferedReader in = null;
@@ -453,6 +383,7 @@ public class DataAnalysis {
 	
 	
 	private static TreeSet<String> getTickersFromFile(String fileName, int colNum){
+
 		TreeSet<String> tickers = new TreeSet<String>();
 		BufferedReader in = null;
 		try{
@@ -477,4 +408,38 @@ public class DataAnalysis {
 		
 		return tickers;
 	}
+	
+	//for an ArrayList of CIKS, start date and numQuarters
+	//return a Hashtable of CIKS to returns, if the CIK does not have data for numQuarters the return value is -1.
+	
+	private static Hashtable<CIK, Double> computeReturns(TreeSet<CIK> ciks, Quarter startQuarter, int numQuarters){
+		Lib.assertTrue(numQuarters%4 ==  0);
+		
+		Hashtable<CIK, Double> cikToReturn = new Hashtable<CIK, Double>();
+		
+		DB.turnMultipleQueriesOn();
+		
+		Quarter endQuarter = startQuarter;
+		for(int i = 0; i < numQuarters; i++)
+			endQuarter = endQuarter.getNextQuarter();
+		
+		double totalRet  = 1;
+		double tempRet = 0.0;
+		for(CIK cik: ciks){
+			for(Quarter q = startQuarter; q.compareTo(endQuarter) <= 0; q = q.getNextQuarter()){
+				tempRet = DB.getFundReturn(cik, q);
+				if(tempRet == -999){ // if there is no return for that quarter
+					totalRet = -999;
+					break;
+				}
+				totalRet *= (tempRet+1);
+			}
+			cikToReturn.put(cik, totalRet-1);
+			totalRet = 1;
+		}
+		
+		DB.turnMultipleQueriesOff();
+		return cikToReturn;
+	}
+	
 }
